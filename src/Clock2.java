0d050beb54ae6f23d9165d87019b3dc14fae9fd4
MeniViner <MeniViner@gmail.com>
2023-06-08 21:56:48 +0300

    clock

---
+++ src/Clock2.java
@@ -0,0 +1,28 @@
+public class Clock2 {
+    private int hour; //23
+    private int min; //59
+
+    public Clock2(int hour, int min) {
+        this.hour = hour;
+        this.min = min;
+    }
+    public int getHour() {
+        return hour;
+    }
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+    public int getMin() {
+        return min;
+    }
+    public void setMin(int min) {
+        this.min = min;
+    }
+    public int getInterval(){
+        int time = 0;
+        time += 23 - (hour * 60);
+        time += 59 - (min);
+        return time;
+    }
+
+}
