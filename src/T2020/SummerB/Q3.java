f0c535566aa9401575fb82095a8339f791a642f2
MeniViner <MeniViner@gmail.com>
2023-06-11 15:52:41 +0300

    mha'at

---
+++ src/T2020/SummerB/Q3.java
@@ -0,0 +1,26 @@
+package T2020.SummerB;
+
+public class Q3 {
+
+    public static int what(int a, int b) {
+        int c = 0;
+        while (a > 0) {
+            c = c + b;
+            a--;
+        }
+        return c;
+    }
+
+    public static int why(int x, int y) {
+        int z = 1;
+        while (y > 0) {
+            z = what(z, x);
+            y--;
+        }
+        return z;
+    }
+
+    public static void main(String[] args) {
+        what(4, 3);
+    }
+}
