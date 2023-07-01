74e4338c4dac9df195f5d80b93c6d66d33ae52eb
MeniViner <meniviner@gmail.com>
2023-07-01 15:26:22 +0300

    mha'at

---
+++ src/T2022/SummerB/q12/Book.java
@@ -0,0 +1,59 @@
+package T2022.SummerB.q12;
+
+public class Book {
+    private int code;
+    private String name;
+    private String genre;
+    private int numOfCopies;
+    private int[] rating;
+
+    public Book(int code, String name, String genre, int numOfCopies, int[] rating) {
+        this.code = code;
+        this.name = name;
+        this.genre = genre;
+        this.numOfCopies = numOfCopies;
+        this.rating = rating;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getNumOfCopies() {
+        return numOfCopies;
+    }
+
+    public void setNumOfCopies(int numOfCopies) {
+        this.numOfCopies = numOfCopies;
+    }
+
+    public int[] getRating() {
+        return rating;
+    }
+
+    public void setRating(int[] rating) {
+        this.rating = rating;
+    }
+
+
+}
---
+++ src/T2022/SummerB/q12/Library.java
@@ -0,0 +1,4 @@
+package T2022.SummerB.q12;
+
+public class Library {
+}
